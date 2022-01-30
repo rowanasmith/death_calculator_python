f3ed1c543121de92ccfe1c3e7d0ecdfc2184b930
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-01-30 10:41:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+days = 32850 - (int(age) * 365)
+weeks = 4680 - (int(age) * 52)
+months = 1080 - (int(age) * 12)
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left before you die.")
+
+
+
+
+
+
+
